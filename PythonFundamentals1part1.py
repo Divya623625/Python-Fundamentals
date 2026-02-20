fc9e67f0b694338558448c4d94c56ed80258b4c7
Divya P <divyadivyap9817@gmail.com>
2026-02-20 18:56:50 +0530

    Added First Python Practice File

---
+++ PythonFundamentals1part1.py
@@ -0,0 +1,42 @@
+"""
+Python Fundamentals - Print Function Practice
+This File contains:
+-Basic print statements
+-Understanding of Function and string
+-IPO Model [Input-Process-Output]
+-Character Set in Python
+"""
+print("Hello World!")
+print("Apna college")
+print("Hello world","with python")
+print("hello \nworld","with python")
+print("Welcome to Apna College")
+print("Hello World with Python!")
+print("Hello\nworld","With Python")
+
+"""
+First Program in Python
+print("Hello World")
+  |             |      
+Function     String
+
+IPO Model (Input -> Process -> Output)
+Input - Data given to the program
+  |
+Code - Logic written inside program
+  |
+Output - Result displayed to user
+
+Character Set In Python
+Character set means all characters that python allows us to use in a program
+- Letters : A-Z , a-z
+- Digits: 0-9
+- Special Symbols: +,-,*,/,% etc.
+- Whitespaces: Blank space, tab[\t], newline etc.
+- All ASCII and Unicode characters as part of data and literals
+
+Filename: PythonFundamentals1part1.py
+Topic: Python Print Function And Basics
+Author: Divya P
+Date: 20-02-2026
+"""
