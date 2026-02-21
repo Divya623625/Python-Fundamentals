d03a5fb62c9de500f500ff241026a19532ff2d0e
Divya P <divyadivyap9817@gmail.com>
2026-02-21 19:17:24 +0530

    Added range() function

---
+++ PythonFundamentals2/PythonFundamentals2part20.py
@@ -0,0 +1,17 @@
+# range() function sequence generate 
+# range(start, stop, step)
+# range(5) - 0 to 4 - It keeps on increasing +1
+# range(1,6) => 1,2,3,4,5
+# range(1,10,2) => 1,3,5,7,9
+
+for i in range(5):
+    print(i)
+
+for i in range(1,6):
+    print(i)
+
+for i in range(1,11,2):
+    print(i)
+
+for i in range(2,11,2):
+    print(i)
