3297cef442cc4491ed97591ad7e534f4d472c3f9
Divya P <divyadivyap9817@gmail.com>
2026-02-21 14:04:09 +0530

    Added practice question1 with answer

---
+++ PythonFundamentals1/PythonFundamentals1PracticeQuestion1.py
@@ -0,0 +1,5 @@
+# Q1. Write a program that asks the user for their name and age, then prints a sentence like:
+# Hello Divya, you are 21 years old!
+Name=input("Enter your name: ")
+age=int(input("Enter your age: "))
+print("Hello",Name,", you are", age, "Years old!")
