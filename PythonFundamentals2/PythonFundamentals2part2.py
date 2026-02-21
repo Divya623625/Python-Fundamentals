35860a9dc22dca81aba12c91ebca2e4fe8752709
Divya P <divyadivyap9817@gmail.com>
2026-02-21 15:27:18 +0530

    Added if else example

---
+++ PythonFundamentals2/PythonFundamentals2part2.py
@@ -0,0 +1,8 @@
+# If-else Condition - True - if block execution orelse False - else block execution
+# If-else is used to check a condition and run one block if it passes, another if it fails
+age=int(input("Enter age: "))
+if age>=18:
+    print("You can vote")
+    print("You can drive")
+else:
+    print("You can't vote")
