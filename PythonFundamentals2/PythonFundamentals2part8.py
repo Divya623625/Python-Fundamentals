fe1280f4565d6e93a1aa264eeab49c93c0c7acbd
Divya P <divyadivyap9817@gmail.com>
2026-02-21 19:32:15 +0530

    Added function examples

--- PythonFundamentals2/PythonFundamentals2part8.py
+++ PythonFundamentals2/PythonFundamentals2part8.py
@@ -1,18 +1,44 @@
 """
-​"Write a Python program that asks for a username and password. 
-Use nested if-else statements so that if the login fails, the program 
-specifically checks and prints if 'Both are wrong', or if it is just a 
-'Wrong username' or a 'Wrong password'."""
-Username=input("Enter Username: ")
-Password=input("Enter Password: ")
+Built-in and user-defined
+print()       sum
+input()       calc_avg
+type()        ......
+range()       ......
+range(start, stop, step) => Optional
+lambda a,b,c:  _____a+b+c => Expression
+Lambda function (Python) 
+A lambda function is a small, one-line function without a name.
+Used when you need a function only once.
+lambda arguments : expression
+"""
+sum=lambda a,b: a+b
+print(sum(4,5))
 
-if Username == "Admin" and Password == "pass":
-    print("Login Successfull")
-else:
-    if Username!="Admin" and Password!="pass":
-        print("Both Username and password are wrong")
-    else:
-        if(Username != "Admin"):
-                print("Wrong username")
-        else:
-                print("Wrong password")
+avg=lambda a,b: (a+b)/2
+print(avg(4,5))
+
+# usage of lambda => High order fnx
+# () => fnx
+# return => fnx 
+# def fun():
+#   ________
+#   ________ 
+
+# WAF to print factorial of n:
+# n=5
+# 1*2*3*4*5 => 120
+# 5!=120
+# n!=>1*2*3*4*5*6.....*n
+# fact = 1
+# for i in range(1,n+1):
+#    fact=fact*i
+# print
+
+def calc_factorial(n):
+    fact=1
+    for i in range(1,n+1):
+        fact*=i
+    return fact
+
+n=int(input("Enter n: "))
+print(calc_factorial(n))
