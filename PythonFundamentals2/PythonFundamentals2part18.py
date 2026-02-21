96c2d309554bc8d33f3c96947e737180b8c347ba
Divya P <divyadivyap9817@gmail.com>
2026-02-21 19:11:52 +0530

    Added practice question on loops with example

---
+++ PythonFundamentals2/PythonFundamentals2part18.py
@@ -0,0 +1,9 @@
+# Count the number of i's in the word artificial intelligence"
+word="artificial intelligence"
+#count the number of i's => 5
+count=0
+for ch in word:
+    if(ch=='i'):
+      count+=1
+
+print("count of i=", count)
