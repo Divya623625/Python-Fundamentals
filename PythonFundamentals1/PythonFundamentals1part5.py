1edfaab530ab67386876bcb8c21f209ff070693e
Divya P <divyadivyap9817@gmail.com>
2026-02-21 13:17:08 +0530

    Updated Variable naming conventions and sum program

--- PythonFundamentals1/PythonFundamentals1part5.py
+++ PythonFundamentals1/PythonFundamentals1part5.py
@@ -1,21 +1,15 @@
 ''' 
-#Snake_case : Words are seperated using _ , lowercase letters : Example - tot_price=100
-#Camelcase : First word is small, next words start with capital letters : Example - totPrice 
-#Pascalcase : All words start with capital letters : Example - TotPrice 
+# Snake_case : Words are seperated using _ , lowercase letters : Example - tot_price=100
+# Camelcase : First word is small, next words start with capital letters : Example - totPrice 
+# Pascalcase : All words start with capital letters : Example - TotPrice 
 Always prefer snakecase
 '''
 tot_price =100
 full_name = "Divya P"
 
-#Write a program to calculate sum of 2 numbers
+# Write a program to calculate sum of 2 numbers
 a = 5
 b = 10
 sum = a + b
 print(sum)
 
-"""
-Filename: PythonFundamentals1part5.py
-Topic: Python Variable naming conventions
-Author: Divya P
-Date: 20-02-2026
-"""
