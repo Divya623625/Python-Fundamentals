4feb0f1ced02f656147ab94747eb0eb15f956a1d
Divya P <divyadivyap9817@gmail.com>
2026-02-21 15:52:16 +0530

    Added Conditional Statement example 3

---
+++ PythonFundamentals2/PythonFundamentals2part6.py
@@ -0,0 +1,6 @@
+# Write a python program that takes a number as input and checks whether it is a multiple of 5 or not
+n=int(input("Enter number: "))
+if (n % 5==0):
+    print("Multiple of 5")
+else:
+    print("Not multiple of 5")
