d6b5ea83af2b2bc0a3e60cd44c891d559c2233d7
Divya P <divyadivyap9817@gmail.com>
2026-02-21 07:30:08 +0530

    Added Keywords, comments

---
+++ PythonFundamentals1/PythonFundamentals1part4.py
@@ -0,0 +1,67 @@
+"""
+Keywords are reserved words which cannot be used as variable names
+1.False
+2.None
+3.True
+4.and
+5.as
+6.assert
+7.async
+8.def
+9.del
+10.elif
+11.else
+12.await
+13.break
+14.class
+15.continue
+16.except
+17.finally
+18.for
+19.in 
+20.is
+21.lambda
+22.match
+23.nonlocal
+24.not
+25.or
+26.while
+27.with
+28.yield
+29.case
+30.from
+31.global
+32.if
+33.import
+34.pass
+35.raise
+36.return
+37.try
+"""
+# Comments are used to explain code. Python ignores them during execution.
+# 1. Single-line comment
+# This is a single-line comment
+x = 10  #assigning value to x
+print(x)  
+False = 123
+print(False)
+
+# 2. Multi-line comment
+"""
+This is a multi-line comment
+used for explanations
+"""
+
+# (or)
+
+'''
+Multi-line comment
+example
+'''
+"""
+
+3. Why comments are used
+=> To explain code
+=> To make code easy to understand
+=> For debugging (temporarily disabling code)
+"""
