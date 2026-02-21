3c6190288137600a6aa74eb527ca43f9e6ce24aa
Divya P <divyadivyap9817@gmail.com>
2026-02-21 14:18:00 +0530

    Added Practice question8 with answer

---
+++ PythonFundamentals1/PythonFundamentals1PracticeQuestion8.py
@@ -0,0 +1,5 @@
+# Q8. Take the radius (r) as user input and print the area.
+# Use the formula: Area= PI * r2 (value of PI=3.14)
+radius=float(input("Enter the radius: "))
+area = 3.14 * radius * radius
+print(area)
