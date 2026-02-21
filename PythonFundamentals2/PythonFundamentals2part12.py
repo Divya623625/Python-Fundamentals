edcc172a833f9882cd63dbea94c00ea8db086e89
Divya P <divyadivyap9817@gmail.com>
2026-02-21 18:46:21 +0530

    Added Loop practice question

---
+++ PythonFundamentals2/PythonFundamentals2part12.py
@@ -0,0 +1,7 @@
+# Write a python program that takes a number as input and prints its multiplication 
+# table from 1 to 10 using while loop
+n=int(input("Enter a number: "))
+i = 1
+while(i<=10):
+    print(n * i)
+    i += 1
