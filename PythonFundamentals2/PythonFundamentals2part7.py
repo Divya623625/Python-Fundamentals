d934fa928a80e766c86771142a12789a362ea5c3
Divya P <divyadivyap9817@gmail.com>
2026-02-21 15:54:52 +0530

    Added Conditional Statement example 4

---
+++ PythonFundamentals2/PythonFundamentals2part7.py
@@ -0,0 +1,6 @@
+# Write a python program that takes an integer as input and determines if it is an even or odd number
+n=int(input("Enter the number: "))
+if n % 2==0:
+    print("Even")
+else:
+    print("Odd")
