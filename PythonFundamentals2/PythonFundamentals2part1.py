06cb1defcb46cdd8b7d15438868f629250ee0f86
Divya P <divyadivyap9817@gmail.com>
2026-02-21 15:23:27 +0530

    Added practiced if example

---
+++ PythonFundamentals2/PythonFundamentals2part1.py
@@ -0,0 +1,7 @@
+# If is a conditional statement used to execute code only when a condition is true.
+# Condition must return true or false
+# Indentation (spaces) is mandatory in Python
+age=21
+if age>=18:
+    print("You can vote")
+    print("You can drive")
