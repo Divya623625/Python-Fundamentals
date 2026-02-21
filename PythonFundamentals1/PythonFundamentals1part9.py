b73f9103e5da146eba63342c3fc03cf5060e2529
Divya P <divyadivyap9817@gmail.com>
2026-02-21 13:42:52 +0530

    Added input() function and sum program

---
+++ PythonFundamentals1/PythonFundamentals1part9.py
@@ -0,0 +1,18 @@
+#input() - Taking input from user
+a = input("Enter value of a: ")
+print(a)
+
+username = input("Enter your name: ")
+print("Welcome", username)
+
+#Sum of 2 Numbers
+c=int(input("Enter c: "))
+d=int(input("Enter d: "))
+sum = c+d
+print(sum)
+
+e=float(input("Enter c: "))
+f=float(input("Enter d: "))
+sum1 = e+f
+print(sum1)
+
