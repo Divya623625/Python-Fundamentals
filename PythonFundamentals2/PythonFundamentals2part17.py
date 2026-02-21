0cd2020e30406aa4269168bb0b0271f52f77450e
Divya P <divyadivyap9817@gmail.com>
2026-02-21 19:09:36 +0530

    Added range() function with examples

---
+++ PythonFundamentals2/PythonFundamentals2part17.py
@@ -0,0 +1,9 @@
+# for i in range(5) - max value - It runs from 0 to 4 - sequence
+for i in range(5):
+    print(i)
+
+for i in range(5):
+    print(i+1)
+
+for i in range(5):
+    print("Hello world!")
