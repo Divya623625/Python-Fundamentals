450a45cfab9a751679280349a7fd924e0ee9f3ce
Divya P <divyadivyap9817@gmail.com>
2026-02-21 14:05:39 +0530

    Added Practice Question2 with answer

---
+++ PythonFundamentals1/PythonFundamentals1PracticeQuestion2.py
@@ -0,0 +1,7 @@
+# Q2. Take two numbers as input from the user and print their sum, difference, product and quotient.
+num1=int(input("Enter the 1st Value: "))
+num2=int(input("Enter the 2nd Value: "))
+print("The sum of",num1,"and",num2,"is",num1+num2)
+print("The difference of",num1,"and",num2,"is",num1-num2)
+print("The product of",num1,"and",num2,"is",num1*num2)
+print("The quotient of",num1,"and",num2,"is",num1/num2)
