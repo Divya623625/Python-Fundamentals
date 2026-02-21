f049ebe376f6169cd70caca59d16550ff86843f9
Divya P <divyadivyap9817@gmail.com>
2026-02-21 18:42:29 +0530

    Added practice loop question with answer

---
+++ PythonFundamentals2/PythonFundamentals2part11.py
@@ -0,0 +1,16 @@
+"""
+Write a python program that uses while loops to:
+1. Print numbers from 1 to 5 in order
+2. Print numbers from 10 down to 1 in reverse"""
+
+#Finite loop
+i = 1 #iterator
+while (i<=5):
+    print(i)
+    i+=1
+
+# Print reverse
+i = 10
+while (i>=1):
+    print(i)
+    i-=1
