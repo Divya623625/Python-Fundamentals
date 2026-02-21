75177230cd7c891913d33a5d0283c0ed117ceed2
Divya P <divyadivyap9817@gmail.com>
2026-02-21 16:23:52 +0530

    Added loop example 1

---
+++ PythonFundamentals2/PythonFundamentals2part10.py
@@ -0,0 +1,8 @@
+#​ Write a Python program using a while loop to print 'Hello world!' exactly 5 times, showing 
+# the counter number on each line.
+counter=1 #iterator you can use i,j,k,l
+while (counter<=5): # 1,2,3,4,5
+    print("Hello world!",counter)
+    counter+=1
+
+print("after loop, count=",counter) #6
