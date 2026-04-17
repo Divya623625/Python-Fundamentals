27563d4289c328fd273e5ba2822894dec95c7258
Divya P <divyadivyap9817@gmail.com>
2026-04-17 19:23:48 +0530

    Added loop to print numbers from 1 to 5 and reverse from 5 to 1

---
+++ PythonFundamentals2/PythonFundamentals2part11.py
@@ -0,0 +1,15 @@
+# Print numbers from 1 to 5
+# finite loop => 5x
+i = 1 # iterator
+while i <= 5:
+    print(i,end=" ")
+    i += 1
+
+print("\n=========")
+
+# Print numbers from reverse 5 to 1
+# finite loop => 5x
+j = 5 # iterator
+while j >= 1:
+    print(j,end=" ")
+    j -= 1
