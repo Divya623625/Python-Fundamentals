07d9ec4a3164bb5d43c082a365df165d9dc3b067
Divya P <divyadivyap9817@gmail.com>
2026-04-17 19:22:53 +0530

    Added loop to print 'Hello World 10 times

---
+++ PythonFundamentals2/PythonFundamentals2part10.py
@@ -0,0 +1,29 @@
+"""
+print("Hello World") 100x
+print("Hello World")
+print("Hello World")
+print("Hello World")
+print("Hello World")
+print("Hello World")
+print("Hello World")
+print("Hello World")
+print("Hello World")
+print("Hello World")
+print("Hello World")
+
+🔁 What is a Loop?
+A loop is used to repeat a block of code again and again.
+👉 Example: Print numbers 1 to 5 without writing 5 print statements.
+
+# Infinite loop
+while True:
+    print("Hello World")
+"""
+# Finite loop
+# Write a Python Program to print Hello World 10 times
+count = 1   # iterator
+while count <= 10:
+    print("Hello World")
+    count += 1
+
+print("After Loop, count=", count) 
