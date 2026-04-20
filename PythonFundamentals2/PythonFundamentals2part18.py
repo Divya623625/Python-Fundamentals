a372046b99e52cb078c882d1813a837c38868013
Divya P <divyadivyap9817@gmail.com>
2026-04-20 15:04:23 +0530

    Python program to demonstrate the range() function

---
+++ PythonFundamentals2/PythonFundamentals2part18.py
@@ -0,0 +1,23 @@
+# Range Function In Python
+
+# Print 0 to 4
+for i in range(5):
+    print(i , end = " ")
+
+print()
+
+# Print odd numbers from 1 to 9
+for j in range(1,10,2):
+    print(j , end = " ")
+
+print()
+
+# Print 1 to 5
+for k in range(1,6):
+    print(k , end = " ")
+
+print()
+
+# Print even numbers from 2 to 10
+for l in range(2,11,2):
+    print(l , end = " ")
