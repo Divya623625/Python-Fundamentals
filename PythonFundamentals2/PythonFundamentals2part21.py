e898f351fe7bec45542f99d029c15a492e46faed
Divya P <divyadivyap9817@gmail.com>
2026-04-20 15:07:44 +0530

    Added function to find the sum of 2 numbers

---
+++ PythonFundamentals2/PythonFundamentals2part21.py
@@ -0,0 +1,10 @@
+# Write a function to find the sum of 2 numbers in Python
+
+# function definition
+def sum(a,b):
+    sum = a + b
+    return sum
+
+# function call
+print(sum(5,6))
+print(sum(500,500))
