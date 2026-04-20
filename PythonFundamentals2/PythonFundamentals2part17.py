861fdea5336a967988f05907e2f1687a75f2edd8
Divya P <divyadivyap9817@gmail.com>
2026-04-20 15:00:36 +0530

    Added Python program to count the number of vowels in the word artificial intelligence

---
+++ PythonFundamentals2/PythonFundamentals2part17.py
@@ -0,0 +1,7 @@
+# Write a Python Program to count the number of vowels in the word Artificial Intelligence
+word = "artificial intelligence"
+count = 0
+for ch in word:
+    if ch=='a' or ch=='e' or ch=='i' or ch=='o' or ch=='u':
+        count += 1
+print("Number of vowels in the word artificial intelligence = ",count)
