9a34cc9fc35ec6e374ff222a36637e11614b66ec
Divya P <divyadivyap9817@gmail.com>
2026-04-20 15:06:33 +0530

    Python program to demonstrate the function definition and function call

---
+++ PythonFundamentals2/PythonFundamentals2part20.py
@@ -0,0 +1,20 @@
+# In Python, a function is a reusable block of code that performs a specific task. 
+# Functions help you organize code, avoid repetition, and make programs easier to 
+# read and maintain.
+
+# Types Of Functions
+# Built-in functions:
+# These are pre-defined functions provided by Python, so you don’t need to write their code. 
+# Examples: print, len, max, min, sum
+
+# User-defined functions:
+# These are functions created by the programmer using def to perform a specific task based 
+# on requirement. They help in code reuse and better organization of programs.
+# Examples: add, greet, calculate, total
+
+def hello(): # fnx definition
+    print("Hello")
+    print("From python")
+
+hello() # fnx call
+hello() # fnx call
