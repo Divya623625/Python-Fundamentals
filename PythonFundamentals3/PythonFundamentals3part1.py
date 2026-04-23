0a36299c413b7392305d2ef40da0101bcf2dd211
Divya P <divyadivyap9817@gmail.com>
2026-04-23 18:34:05 +0530

    Added Python Strings basics(length,concatenation,indexing,traversing)

---
+++ PythonFundamentals3/PythonFundamentals3part1.py
@@ -0,0 +1,26 @@
+# In Python, a string is a sequence of characters used to represent text. 
+# Strings are one of the most commonly used data types.
+# String is immutable = once created, you cannot modify it
+
+word1 = "Hello"
+word2 = "Python"
+
+# Length of the string
+print(len(word1))
+
+# Concatenation
+print(word1 + " " + word2)
+ 
+# Indexing
+print(word1[0])
+print(word1[1])
+print(word1[2])
+print(word1[3])
+print(word1[4])
+
+# Traversing a string
+for ch in word1:
+    print(ch ,end = " ")
+
+
+
