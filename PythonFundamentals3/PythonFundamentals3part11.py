3423e5539eeb4b221c263c41e3e546622a1033c3
Divya P <divyadivyap9817@gmail.com>
2026-04-23 18:59:22 +0530

    Added Sets basics in Python

---
+++ PythonFundamentals3/PythonFundamentals3part11.py
@@ -0,0 +1,15 @@
+# Sets are collection of unique elements (no duplicates)
+
+s = {1,2,3,4,5,6,3,3,2,2}
+print(s)
+print(type(s))
+print(len(s))
+
+s.add(10)
+print(s)
+
+empty_set = {}
+print(type(empty_set))
+
+empty_set1 = set()
+print(type(empty_set1))
