2e452b1acf9f8d8831018909a7f3c0fbef47429b
Divya P <divyadivyap9817@gmail.com>
2026-04-23 18:35:20 +0530

    Added Python string slicing examples

---
+++ PythonFundamentals3/PythonFundamentals3part2.py
@@ -0,0 +1,16 @@
+# Slicing = taking a part of a string
+
+# string[start : end]
+# start = where to begin
+# end = where to stop (end not included)
+
+str = "Python"
+print(str[2:5])
+
+word = "I study from apna college"
+print(word[13:25])
+print(word[13:])
+print(word[:len(word)])
+
+word1 = "Hello"
+print(word[-4:-2])
