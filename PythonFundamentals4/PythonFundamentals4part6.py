7fc5f2135d8599c17ee4074409d6a75926381af7
Divya P <divyadivyap9817@gmail.com>
2026-04-26 23:43:54 +0530

    Added Python program demonstrating Encapsulation

---
+++ PythonFundamentals4/PythonFundamentals4part6.py
@@ -0,0 +1,18 @@
+# Encapsulation => Wrapping data and functions into single unit
+class BankAccount:
+    def __init__(self, name, balance):
+        self.name = name # public
+        self.__balance = balance # private - data mangling
+    
+    def get_balance(self): # getter
+        return self.__balance
+    
+    def set_balance(self, newBalance): # setter
+        self.__balance = newBalance
+
+acc1 = BankAccount("Rahul kumar", "100_000")
+
+acc1.set_balance(200_000)
+print(acc1.name, acc1.get_balance())
+print(acc1.name, acc1._BankAccount__balance)
+    
